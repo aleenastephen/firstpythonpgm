bf889ca4cf45c2ee45d5a17510871475a4239c95
aleenastephen <aleenastephen95@gmail.com>
2019-09-30 14:49:28 +0530

    First Django project

---
+++ mzc/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def indexpage(request):
+    return HttpResponse("Welcome to My Index Page")
+def contactpage(request):
+    return HttpResponse("Contact Page")  
+def homepage(request):
+    return HttpResponse("Welcome to My Home Page")      
+
