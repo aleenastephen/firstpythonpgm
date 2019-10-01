a504a9c245338e53423a78c5053cc2d628884a1b
aleenastephen <aleenastephen95@gmail.com>
2019-10-01 16:04:23 +0530

    completed

--- mzc/models.py
+++ mzc/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+GENDER_CHOICE=(
+    ('M','MALE'),
+    ('F','FEMALE'),
+    ('O','OTHERS')
+)
+class Students(models.Model):
+    name=models.CharField(max_length=30)
+    rollno=models.CharField(max_length=30)
+    address=models.TextField()
+    course=models.CharField(max_length=40)
+    gender=models.CharField(max_length=1,choices=GENDER_CHOICE,default='M')
+
+class Faculty(models.Model):
+    name=models.CharField(max_length=30)
+    designation=models.CharField(max_length=30)
+    address=models.TextField()
+    subject=models.CharField(max_length=30)
+    gender=models.CharField(max_length=1,choices=GENDER_CHOICE,default='M')
--- mzc/views.py
+++ mzc/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import FacultyForm
 # Create your views here.
 def indexpage(request):
     return render(request,'index.html')
@@ -7,4 +8,7 @@ def contactpage(request):
     return render(request,'contact.html')  
 def homepage(request):
     return render(request,'home.html')      
+def facultypage(request):
+    form=FacultyForm()
+    return render(request,'faculty.html',{'form':form})
 
